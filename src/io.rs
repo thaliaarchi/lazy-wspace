9969a5fb593665e551d0c3867b87316899d9414d
Thalia Archibald <thalia@archibald.dev>
2023-03-15 18:47:17 -0600

    Record IO effects

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,6 @@
-use std::io::{BufRead, ErrorKind};
+use std::cell::RefCell;
+use std::io::{self, BufRead, ErrorKind, Read, Write};
+use std::time::{Duration, Instant};
 
 use arrayvec::ArrayVec;
 
@@ -54,5 +56,108 @@ impl<R: BufRead> CharReader<R> {
             Err(err) if err.kind() == ErrorKind::InvalidData => Err(Error::ReadInvalidUtf8),
             Err(err) => Err(err.into()),
         }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct IoRecorder {
+    effects: RefCell<Vec<(Effect, Duration)>>,
+    start: Instant,
+}
+
+#[derive(Clone, Debug)]
+pub struct StdinRecorder<'a, R> {
+    r: R,
+    rec: &'a IoRecorder,
+}
+
+#[derive(Clone, Debug)]
+pub struct StdoutRecorder<'a> {
+    rec: &'a IoRecorder,
+}
+
+#[derive(Clone, Debug)]
+pub struct StderrRecorder<'a> {
+    rec: &'a IoRecorder,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Effect {
+    Read(Result<Vec<u8>, io::ErrorKind>),
+    WriteOut(Vec<u8>),
+    WriteErr(Vec<u8>),
+    FlushOut,
+    FlushErr,
+}
+
+impl IoRecorder {
+    #[inline]
+    pub fn new() -> Self {
+        IoRecorder {
+            effects: RefCell::new(Vec::new()),
+            start: Instant::now(),
+        }
+    }
+
+    #[inline]
+    pub fn stdin<R: Read>(&self, r: R) -> StdinRecorder<R> {
+        StdinRecorder { r, rec: self }
+    }
+
+    #[inline]
+    pub fn stdout(&self) -> StdoutRecorder {
+        StdoutRecorder { rec: self }
+    }
+
+    #[inline]
+    pub fn stderr(&self) -> StderrRecorder {
+        StderrRecorder { rec: self }
+    }
+}
+
+impl<R: Read> Read for StdinRecorder<'_, R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let mut effects = self.rec.effects.borrow_mut();
+        let time = self.rec.start.elapsed();
+        let res = self.r.read(buf);
+        let effect = Effect::Read(
+            res.as_ref()
+                .map(|&n| buf[..n].to_owned())
+                .map_err(|err| err.kind()),
+        );
+        effects.push((effect, time));
+        res
+    }
+}
+
+impl Write for StdoutRecorder<'_> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let mut effects = self.rec.effects.borrow_mut();
+        let time = self.rec.start.elapsed();
+        effects.push((Effect::WriteOut(buf.to_owned()), time));
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let mut effects = self.rec.effects.borrow_mut();
+        let time = self.rec.start.elapsed();
+        effects.push((Effect::FlushOut, time));
+        Ok(())
+    }
+}
+
+impl Write for StderrRecorder<'_> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let mut effects = self.rec.effects.borrow_mut();
+        let time = self.rec.start.elapsed();
+        effects.push((Effect::WriteErr(buf.to_owned()), time));
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        let mut effects = self.rec.effects.borrow_mut();
+        let time = self.rec.start.elapsed();
+        effects.push((Effect::FlushErr, time));
+        Ok(())
     }
 }
