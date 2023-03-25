22f86abe925ed28d165088f59307f451d42ac6cf
Thalia Archibald <thalia@archibald.dev>
2023-03-24 18:58:19 -0600

    Fix and test laziness in AbstractStack

--- src/ir/stack.rs
+++ src/ir/stack.rs
@@ -1,33 +1,45 @@
 use crate::error::UnderflowError;
+use crate::inst::NumberLit;
 use crate::ir::{AbstractNumber, AbstractNumberRef};
 
 /// An abstract stack for stack operations in a basic block
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct AbstractStack {
-    under: Vec<Option<AbstractNumberRef>>,
-    on_underflow: UnderflowError,
-    drop_eager: usize,
-    drop_lazy: usize,
     values: Vec<AbstractNumberRef>,
+    under: Vec<Option<AbstractNumberRef>>,
+    drops: usize,
+    slide: LazySize,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum LazySize {
+    Finite(usize),
+    /// A slide amount larger than `usize::MAX`, that always underflows when
+    /// evaluated.
+    Overflow,
+    /// Represents `Slide(NumberLit::Empty)`. When it is evaluated, it produces
+    /// `UnderflowError::SlideEmpty`.
+    EmptyLit,
 }
 
 impl AbstractStack {
     #[inline]
     pub fn new() -> Self {
         AbstractStack {
-            under: Vec::new(),
-            on_underflow: UnderflowError::Normal,
-            drop_eager: 0,
-            drop_lazy: 0,
             values: Vec::new(),
+            under: Vec::new(),
+            drops: 0,
+            slide: LazySize::Finite(0),
         }
     }
 
+    /// Pushes a value to the stack.
     #[inline]
     pub fn push(&mut self, n: AbstractNumberRef) {
         self.values.push(n);
     }
 
+    /// Eagerly pushes a reference to the top element on the stack.
     #[inline]
     pub fn dup(&mut self) -> Result<AbstractNumberRef, UnderflowError> {
         let top = self.top()?;
@@ -35,13 +47,16 @@ impl AbstractStack {
         Ok(top)
     }
 
+    /// Eagerly pushes a lazy reference to the nth element on the stack.
     #[inline]
     pub fn copy(&mut self, n: usize) -> Result<AbstractNumberRef, UnderflowError> {
+        // TODO: Use a lazy reference
         let nth = self.at(n)?;
         self.push(nth.clone());
         Ok(nth)
     }
 
+    /// Eagerly swaps the top two elements on the stack.
     #[inline]
     pub fn swap(&mut self) -> Result<(), UnderflowError> {
         let x = self.pop()?;
@@ -51,18 +66,21 @@ impl AbstractStack {
         Ok(())
     }
 
+    /// Eagerly accesses the top element of the stack and returns it.
     #[inline]
     pub fn top(&mut self) -> Result<AbstractNumberRef, UnderflowError> {
         self.at(0)
     }
 
+    /// Eagerly accesses the nth element from the top of the stack and returns
+    /// it.
     pub fn at(&mut self, n: usize) -> Result<AbstractNumberRef, UnderflowError> {
         if n < self.values.len() {
             Ok(self.values[self.values.len() - n - 1].clone())
         } else {
-            let i = (self.drop_eager.checked_add(self.drop_lazy))
-                .and_then(|d| d.checked_add(n - self.values.len()))
-                .ok_or(self.on_underflow)?;
+            let n = n - self.values.len();
+            self.eval_slide()?;
+            let i = self.drops.checked_add(n).ok_or(UnderflowError::Normal)?;
             if i >= self.under.len() {
                 self.under.resize(i + 1, None);
             }
@@ -72,78 +90,99 @@ impl AbstractStack {
         }
     }
 
+    /// Eagerly removes the top element from the stack and returns it.
     #[inline]
     pub fn pop(&mut self) -> Result<AbstractNumberRef, UnderflowError> {
         let top = self.top()?;
-        self.drop(1)?;
+        self.drop_eager(1)?;
         Ok(top)
     }
 
-    pub fn drop(&mut self, n: usize) -> Result<(), UnderflowError> {
-        if n < self.values.len() {
+    /// Eagerly removes the top `n` elements from the stack.
+    #[inline]
+    pub fn drop_eager(&mut self, n: usize) -> Result<(), UnderflowError> {
+        if n <= self.values.len() {
             self.values.truncate(self.values.len() - n);
         } else {
-            // Forces evaluation of lazy drops
-            self.drop_eager = (self.drop_eager.checked_add(self.drop_lazy))
-                .and_then(|d| d.checked_add(n - self.values.len()))
-                .ok_or(self.on_underflow)?;
-            self.drop_lazy = 0;
+            let n = n - self.values.len();
             self.values.clear();
+            self.eval_slide()?;
+            self.drops = self.drops.checked_add(n).ok_or(UnderflowError::Normal)?;
         }
         Ok(())
     }
 
-    pub fn drop_lazy(&mut self, n: usize) {
-        if n < self.values.len() {
-            self.values.truncate(self.values.len() - n);
-        } else {
-            self.drop_lazy = self.drop_lazy.saturating_add(n - self.values.len());
-            self.values.clear();
+    /// Lazily removes the top `n` elements from the stack
+    #[inline]
+    pub fn drop_lazy(&mut self, n: LazySize) {
+        match n {
+            LazySize::Finite(n) => {
+                if n <= self.values.len() {
+                    // Eagerly drop pushed values
+                    self.values.truncate(self.values.len() - n);
+                } else {
+                    let n = n - self.values.len();
+                    self.values.clear();
+
+                    // Eagerly drop already-accessed values
+                    let nu = n.min(self.under.len().saturating_sub(self.drops));
+                    self.drops -= nu;
+                    let n = n - nu;
+
+                    // Lazily drop the rest
+                    self.slide = self.slide.combine(LazySize::Finite(n));
+                }
+            }
+            LazySize::Overflow | LazySize::EmptyLit => {
+                // Eagerly drop all values and underflow on further accesses
+                self.values.clear();
+                self.drops = self.drops.max(self.under.len());
+                self.slide = self.slide.combine(n);
+            }
         }
     }
 
-    #[inline]
-    pub fn slide(&mut self, n: usize) -> Result<(), UnderflowError> {
+    /// Lazily removes `n` elements from the top of the stack, keeping the
+    /// topmost element.
+    pub fn slide(&mut self, n: LazySize) -> Result<(), UnderflowError> {
         let top = self.pop()?;
         self.drop_lazy(n);
         self.push(top);
         Ok(())
     }
 
-    #[inline]
-    pub fn slide_empty(&mut self) -> Result<(), UnderflowError> {
-        let top = self.pop()?;
-        self.drop_eager = self.drop_eager.max(self.under.len());
-        self.drop_lazy = usize::MAX;
-        self.on_underflow = UnderflowError::SlideEmpty;
-        self.values.clear();
-        self.push(top);
+    /// Forces evaluation of slide, to perform an eager operation.
+    fn eval_slide(&mut self) -> Result<(), UnderflowError> {
+        self.drops = (self.slide.as_usize()?)
+            .checked_add(self.drops)
+            .ok_or(UnderflowError::Normal)?;
+        self.slide = LazySize::Finite(0);
         Ok(())
     }
 
+    /// Simplifies pushed values that do not change the contents of the stack
+    /// and clears unused stack references.
     pub fn simplify(&mut self) {
-        // Eagerly evaluate lazy drops for any already-accessed values
-        if self.drop_eager < self.under.len() && self.drop_lazy != 0 {
-            let n = self.drop_lazy.min(self.under.len() - self.drop_eager);
-            self.drop_eager += n;
-            self.drop_lazy -= n;
-        }
         // Simplify pop-push identities
-        let dropped = self.drop_eager.min(self.under.len()).min(self.values.len());
-        let mut shift = 0;
-        for i in 0..dropped {
-            if let Some(u) = self.under[dropped - i - 1].as_ref() {
-                if u.same_ref(&self.values[i]) {
-                    shift += 1;
-                    continue;
+        if let Ok(drops_and_slide) = self.slide.add_eager(self.drops) {
+            if drops_and_slide <= self.under.len() {
+                let mut shift = 0;
+                for i in 0..drops_and_slide.min(self.values.len()) {
+                    if let Some(u) = self.under[drops_and_slide - i - 1].as_ref() {
+                        if u.same_ref(&self.values[i]) {
+                            shift += 1;
+                            continue;
+                        }
+                    }
+                    break;
+                }
+                if shift != 0 {
+                    self.values.drain(0..shift);
+                    self.drops -= shift;
                 }
             }
-            break;
         }
-        if shift != 0 {
-            self.values.drain(0..shift);
-            self.drop_eager -= shift;
-        }
+
         // Clear unused stack references
         for i in 0..self.under.len() {
             if let Some(u) = &self.under[i] {
@@ -155,58 +194,164 @@ impl AbstractStack {
     }
 }
 
+impl LazySize {
+    #[inline]
+    pub fn combine(self, rhs: LazySize) -> LazySize {
+        match (self, rhs) {
+            (LazySize::Finite(n), LazySize::Finite(m)) => n
+                .checked_add(m)
+                .map(LazySize::Finite)
+                .unwrap_or(LazySize::Overflow),
+            (LazySize::EmptyLit, _) | (_, LazySize::EmptyLit) => LazySize::EmptyLit,
+            (LazySize::Overflow, _) | (_, LazySize::Overflow) => LazySize::Overflow,
+        }
+    }
+
+    #[inline]
+    pub fn as_usize(&self) -> Result<usize, UnderflowError> {
+        match self {
+            LazySize::Finite(n) => Ok(*n),
+            LazySize::Overflow => Err(UnderflowError::Normal),
+            LazySize::EmptyLit => Err(UnderflowError::SlideEmpty),
+        }
+    }
+
+    #[inline]
+    pub fn add_eager(&self, drops: usize) -> Result<usize, UnderflowError> {
+        self.as_usize()
+            .and_then(|n| n.checked_add(drops).ok_or(UnderflowError::Normal))
+    }
+}
+
+impl From<NumberLit> for LazySize {
+    #[inline]
+    fn from(n: NumberLit) -> Self {
+        match n {
+            NumberLit::Number(n) => {
+                if let Some(n) = n.to_usize() {
+                    LazySize::Finite(n)
+                } else {
+                    LazySize::Overflow
+                }
+            }
+            NumberLit::Empty => LazySize::EmptyLit,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use LazySize::*;
 
     macro_rules! num(($n:expr) => {
         AbstractNumberRef::from(AbstractNumber::Number($n.into()))
     });
-    macro_rules! stack_ref(($n:expr) => {
+    macro_rules! stk(($n:expr) => {
         AbstractNumberRef::from(AbstractNumber::StackRef($n))
     });
-    macro_rules! stack(([$($under:expr),*] => $on_underflow:ident,
-            eager $drop_eager:expr, lazy $drop_lazy:expr, [$($value:expr),*] $(,)?) => {
+    macro_rules! stack(([$($value:expr),*], [$($under:expr),*], $drops:expr, $slide:expr$(,)?) => {
         AbstractStack {
-            under: vec![$($under),*],
-            on_underflow: UnderflowError::$on_underflow,
-            drop_eager: $drop_eager,
-            drop_lazy: $drop_lazy,
             values: vec![$($value),*],
+            under: vec![$($under),*],
+            drops: $drops,
+            slide: $slide,
         }
     });
 
     #[test]
     fn push() {
-        let v0 = num!(0);
-        let mut s = stack!([] => Normal, eager 0, lazy 0, []);
-        let s1 = stack!([] => Normal, eager 0, lazy 0, [v0.clone()]);
-        s.push(v0);
+        let mut s = stack!([], [], 0, Finite(0));
+        let s1 = stack!([num!(0)], [], 0, Finite(0));
+        s.push(num!(0));
         assert_eq!(s1, s);
     }
 
     #[test]
     fn pop() {
-        let v0 = num!(0);
-        let v1 = num!(1);
-        let u0 = stack_ref!(0);
-        let u1 = stack_ref!(1);
-        let u3 = stack_ref!(3);
-
-        let mut s = stack!([] => Normal, eager 0, lazy 0, []);
-        let s1 = stack!([Some(u0.clone())] => Normal, eager 1, lazy 0, []);
-        assert_eq!(u0, s.pop().unwrap());
+        let mut s = stack!([], [], 0, Finite(0));
+        let s1 = stack!([], [Some(stk!(0))], 1, Finite(0));
+        assert_eq!(stk!(0), s.pop().unwrap());
         assert_eq!(s1, s);
 
-        let mut s = stack!([] => Normal, eager 0, lazy 0, [v0.clone(), v1.clone()]);
-        let s1 = stack!([] => Normal, eager 0, lazy 0, [v0.clone()]);
-        assert_eq!(v1, s.pop().unwrap());
+        let mut s = stack!([num!(0), num!(1)], [], 0, Finite(0));
+        let s1 = stack!([num!(0)], [], 0, Finite(0));
+        assert_eq!(num!(1), s.pop().unwrap());
         assert_eq!(s1, s);
 
-        let mut s = stack!([None, Some(u1.clone()), None] => Normal, eager 3, lazy 0, []);
-        let s1 =
-            stack!([None, Some(u1.clone()), None, Some(u3.clone())] => Normal, eager 4, lazy 0, []);
-        assert_eq!(u3, s.pop().unwrap());
+        let mut s = stack!([], [None, Some(stk!(1)), None], 3, Finite(0));
+        let s1 = stack!([], [None, Some(stk!(1)), None, Some(stk!(3))], 4, Finite(0));
+        assert_eq!(stk!(3), s.pop().unwrap());
+        assert_eq!(s1, s);
+    }
+
+    #[test]
+    fn drop_eager() {
+        let mut s = stack!([num!(1), num!(2), num!(3)], [], 5, Finite(3));
+        let s1 = stack!([num!(1)], [], 5, Finite(3));
+        s.drop_eager(2).unwrap();
+        assert_eq!(s1, s);
+
+        let mut s = stack!([num!(1), num!(2), num!(3)], [], 5, Finite(3));
+        let s1 = stack!([], [], 10, Finite(0));
+        s.drop_eager(5).unwrap();
+        assert_eq!(s1, s);
+
+        let mut s = stack!([], [None, Some(stk!(1)), None], 5, Finite(3));
+        let s1 = stack!([], [None, Some(stk!(1)), None], 9, Finite(0));
+        s.drop_eager(1).unwrap();
+        assert_eq!(s1, s);
+
+        let mut s = stack!([], [], 5, Finite(3));
+        let s1 = stack!([], [], 10, Finite(0));
+        s.drop_eager(2).unwrap();
+        assert_eq!(s1, s);
+
+        let mut s = stack!([num!(1), num!(2), num!(3)], [Some(stk!(0))], 5, Overflow);
+        assert_eq!(Err(UnderflowError::Normal), s.drop_eager(5));
+
+        let mut s = stack!([num!(1), num!(2), num!(3)], [Some(stk!(0))], 5, EmptyLit);
+        assert_eq!(Err(UnderflowError::SlideEmpty), s.drop_eager(5));
+
+        let mut s = stack!([num!(1)], [], 3, Finite(usize::MAX - 3));
+        assert_eq!(Err(UnderflowError::Normal), s.drop_eager(5));
+    }
+
+    #[test]
+    fn drop_lazy() {
+        let mut s = stack!([num!(1), num!(2), num!(3)], [], 5, Finite(3));
+        let s1 = stack!([num!(1)], [], 5, Finite(3));
+        s.drop_lazy(Finite(2));
+        assert_eq!(s1, s);
+
+        let mut s = stack!([num!(1), num!(2), num!(3)], [], 5, Finite(3));
+        let s1 = stack!([], [], 5, Finite(5));
+        s.drop_lazy(Finite(5));
+        assert_eq!(s1, s);
+
+        let mut s = stack!([], [None, Some(stk!(1)), None], 5, Finite(3));
+        let s1 = stack!([], [None, Some(stk!(1)), None], 5, Finite(4));
+        s.drop_lazy(Finite(1));
+        assert_eq!(s1, s);
+
+        let mut s = stack!([], [], 5, Finite(3));
+        let s1 = stack!([], [], 5, Finite(5));
+        s.drop_lazy(Finite(2));
+        assert_eq!(s1, s);
+
+        let mut s = stack!([num!(1), num!(2), num!(3)], [Some(stk!(0))], 5, Overflow);
+        let s1 = stack!([], [Some(stk!(0))], 5, Overflow);
+        s.drop_lazy(Finite(5));
+        assert_eq!(s1, s);
+
+        let mut s = stack!([num!(1), num!(2), num!(3)], [Some(stk!(0))], 5, EmptyLit);
+        let s1 = stack!([], [Some(stk!(0))], 5, EmptyLit);
+        s.drop_lazy(Finite(5));
+        assert_eq!(s1, s);
+
+        let mut s = stack!([num!(1)], [], 3, Finite(usize::MAX - 3));
+        let s1 = stack!([], [], 3, Overflow);
+        s.drop_lazy(Finite(5));
         assert_eq!(s1, s);
     }
 }
