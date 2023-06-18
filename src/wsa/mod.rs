9ea8cab337fcbf56c4d02f92cf77ba7536c6f85b
Thalia Archibald <thalia@archibald.dev>
2023-06-18 01:33:40 -0600

    Init with lexing Whitespace assembly

---
+++ src/wsa/mod.rs
@@ -0,0 +1,8 @@
+//! Whitespace assembly syntax.
+//!
+//! This module parses and works with a permissive Whitespace assembly dialect.
+//! It should be able to parse most code of any dialect.
+
+mod lexer;
+
+pub use lexer::*;
