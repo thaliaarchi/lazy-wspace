abbafe28f96bb6f94c94883aeb9c765e8b14cea0
Thalia Archibald <thalia@archibald.dev>
2023-08-28 21:02:04 -0700

    Document the rest of AbstractStack

--- lazy-wspace/src/ir/stack.rs
+++ lazy-wspace/src/ir/stack.rs
@@ -3,11 +3,26 @@ use crate::error::{NumberError, UnderflowError};
 use crate::ir::{Inst, NodeRef, NodeTable};
 use crate::number::Op;
 
-/// Abstract stack for stack operations in a basic block.
-#[derive(Clone, Debug, PartialEq, Eq)]
+/// Abstract stack for stack operations in a scope.
+///
+/// The Whitespace stack is manipulated with a mixture of strict and lazy
+/// evaluation: operands are popped and results are pushed strictly; however,
+/// `copy` accesses the value at its index lazily and `slide` drops under the
+/// top lazily (but accesses the top eagerly). This means, that any errors from
+/// `copy` or `slide`, such as an out-of-bounds index or a parse error, when the
+/// AST operand is an empty integer literal, are encountered lazily.
+/// `AbstractStack` models these semantics abstractly, so the rest of the
+/// compiler can ignore stack invariants.
+///
+/// It is used in IR generation to convert all stack operations in a basic block
+/// to operations on abstract registers. A basic block, then, loads and operates
+/// on values from the incoming stack using fixed offsets. Stack underflow only
+/// needs to be checked when lower values from the incoming stack are accessed.
+/// At the end of the basic block, values are dropped and pushed in a batch.
+#[derive(Debug, PartialEq, Eq)]
 pub struct AbstractStack {
     values: Vec<NodeRef>,
-    // guards[guards.len()-1].0 <= accessed, and is equal until an underflow
+    // guards[guards.len()-1].0 <= accessed, and is == until an underflow
     // occurs.
     guards: Vec<(usize, NodeRef)>,
     accessed: usize, // accessed >= dropped
@@ -15,6 +30,10 @@ pub struct AbstractStack {
     lazy_dropped: LazySize,
 }
 
+/// Stack index or length.
+///
+// TODO: Haskell `Int` is not arbitrary precision, so the `Overflow` variant is
+// not possible.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum LazySize {
     Finite(usize),
@@ -26,6 +45,7 @@ pub enum LazySize {
 }
 
 impl AbstractStack {
+    /// Constructs an empty `AbstractStack`.
     #[inline]
     pub fn new() -> Self {
         AbstractStack {
@@ -37,26 +57,35 @@ impl AbstractStack {
         }
     }
 
+    /// The values pushed to this stack frame.
     #[inline]
     pub fn values(&self) -> &[NodeRef] {
         &self.values
     }
 
+    /// Guards for any values accessed from under this stack frame.
     #[inline]
     pub fn guards(&self) -> &[(usize, NodeRef)] {
         &self.guards
     }
 
+    /// The depth of the values accessed from under this stack frame. Any
+    /// access, excepting unconditional underflows, is guarded. `accessed` is
+    /// always greater than or equal to `dropped`.
     #[inline]
     pub fn accessed(&self) -> usize {
         self.accessed
     }
 
+    /// The number of values popped from under this stack frame.
     #[inline]
     pub fn dropped(&self) -> usize {
         self.dropped
     }
 
+    /// The number of values, that will be popped from under this stack frame
+    /// when the stack is next eagerly evaluated, or an error from the first
+    /// erroneous `slide`.
     #[inline]
     pub fn lazy_dropped(&self) -> LazySize {
         self.lazy_dropped
@@ -69,7 +98,7 @@ impl AbstractStack {
     }
 
     /// Pushes a number to the stack, only cloning it, if it does not already
-    /// exist in the table.
+    /// exist in the table. This corresponds to Whitespace `push`.
     #[inline]
     pub fn push_number(&mut self, n: &NumberLit, table: &mut NodeTable<'_>) -> NodeRef {
         let node = match n {
@@ -80,7 +109,8 @@ impl AbstractStack {
         node
     }
 
-    /// Eagerly pushes a reference to the top element on the stack.
+    /// Eagerly pushes a reference to the top element on the stack. This
+    /// corresponds to Whitespace `dup`.
     #[inline]
     pub fn dup(&mut self, table: &mut NodeTable<'_>) -> Result<(), UnderflowError> {
         let top = self.top(table)?;
@@ -88,7 +118,8 @@ impl AbstractStack {
         Ok(())
     }
 
-    /// Eagerly pushes a lazy reference to the nth element on the stack.
+    /// Eagerly pushes a lazy reference to the nth element on the stack. This
+    /// corresponds to Whitespace `copy`.
     #[inline]
     pub fn copy(&mut self, n: LazySize, table: &mut NodeTable<'_>) {
         let res = match n {
@@ -107,7 +138,8 @@ impl AbstractStack {
         self.push(node);
     }
 
-    /// Eagerly swaps the top two elements on the stack.
+    /// Eagerly swaps the top two elements on the stack. This corresponds to
+    /// Whitespace `swap`.
     #[inline]
     pub fn swap(&mut self, table: &mut NodeTable<'_>) -> Result<(), UnderflowError> {
         let (x, y) = self.pop2(table)?;
@@ -179,7 +211,8 @@ impl AbstractStack {
         }
     }
 
-    /// Eagerly removes the top element from the stack and returns it.
+    /// Eagerly removes the top element from the stack and returns it. This
+    /// corresponds to Whitespace `drop`, but with the dropped node returned.
     #[inline]
     pub fn pop(&mut self, table: &mut NodeTable<'_>) -> Result<NodeRef, UnderflowError> {
         let top = self.top(table)?;
@@ -200,7 +233,8 @@ impl AbstractStack {
     }
 
     /// Eagerly applies an arithmetic operation to the the top two elements on
-    /// the stack.
+    /// the stack. This corresponds to Whitespace `add`, `sub`, `mul`, `div`,
+    /// and `mod`.
     #[inline]
     pub fn apply_op(&mut self, op: Op, table: &mut NodeTable<'_>) -> Result<(), UnderflowError> {
         let (x, y) = self.pop2(table)?;
@@ -217,7 +251,8 @@ impl AbstractStack {
         Ok(())
     }
 
-    /// Eagerly removes the top `n` elements from the stack.
+    /// Eagerly removes the top `n` elements from the stack. This corresponds to
+    /// repeated Whitespace `drop`.
     #[inline]
     pub fn drop_eager(
         &mut self,
@@ -245,7 +280,7 @@ impl AbstractStack {
         Ok(())
     }
 
-    /// Lazily removes the top `n` elements from the stack
+    /// Lazily removes the top `n` elements from the stack.
     #[inline]
     pub fn drop_lazy(&mut self, n: LazySize) {
         match n {
@@ -276,7 +311,7 @@ impl AbstractStack {
     }
 
     /// Lazily removes `n` elements from the top of the stack, keeping the
-    /// topmost element.
+    /// topmost element. This corresponds to Whitespace `slide`.
     pub fn slide(&mut self, n: LazySize, table: &mut NodeTable<'_>) -> Result<(), UnderflowError> {
         let top = self.pop(table)?;
         self.drop_lazy(n);
@@ -285,6 +320,9 @@ impl AbstractStack {
     }
 
     /// Simplifies pushed values, that do not change the contents of the stack.
+    /// It should not be called in a loop, while modifying the stack, as that
+    /// could potentially cause quadratic complexity; instead, call it after a
+    /// batch of modifications, for linear complexity.
     pub fn simplify(&mut self, table: &NodeTable<'_>) {
         // Simplify pop-push identities
         if let Ok(drops) = (self.lazy_dropped.finite())
@@ -309,8 +347,14 @@ impl AbstractStack {
         }
     }
 
+    /// Gets the least [`Inst::GuardStack`] node, that guards at least a stack
+    /// length of `len`, if that length has already been guarded.
     #[inline]
     pub fn get_guard(&self, len: usize) -> Option<NodeRef> {
+        if len > self.accessed {
+            // Short-circuit common case
+            return None;
+        }
         let i = match self.guards.binary_search_by(|(l, _)| l.cmp(&len)) {
             Ok(i) => i,
             Err(i) => i,
@@ -318,6 +362,8 @@ impl AbstractStack {
         self.guards.get(i).map(|&(_, guard)| guard)
     }
 
+    /// Gets the least [`Inst::GuardStack`] node, that guards at least a stack
+    /// length of `len`, or insert a new guard for that length.
     #[inline]
     pub fn get_or_insert_guard(&mut self, len: usize, table: &mut NodeTable<'_>) -> NodeRef {
         match self.get_guard(len) {
@@ -330,6 +376,38 @@ impl AbstractStack {
             }
         }
     }
+
+    /// Resets all state of the stack, reusing memory.
+    #[inline]
+    pub fn clear(&mut self) {
+        self.values.clear();
+        self.guards.clear();
+        self.accessed = 0;
+        self.dropped = 0;
+        self.lazy_dropped = LazySize::Finite(0);
+    }
+}
+
+impl Clone for AbstractStack {
+    #[inline]
+    fn clone(&self) -> Self {
+        AbstractStack {
+            values: self.values.clone(),
+            guards: self.guards.clone(),
+            accessed: self.accessed,
+            dropped: self.dropped,
+            lazy_dropped: self.lazy_dropped,
+        }
+    }
+
+    #[inline]
+    fn clone_from(&mut self, source: &Self) {
+        self.values.clone_from(&source.values);
+        self.guards.clone_from(&source.guards);
+        self.accessed = source.accessed;
+        self.dropped = source.dropped;
+        self.lazy_dropped = source.lazy_dropped;
+    }
 }
 
 #[inline]
@@ -338,6 +416,7 @@ fn add_or_underflow(n: usize, m: usize) -> Result<usize, UnderflowError> {
 }
 
 impl LazySize {
+    /// Converts the number to a finite size, or underflows.
     #[inline]
     pub fn finite(&self) -> Result<usize, UnderflowError> {
         match self {
@@ -347,6 +426,7 @@ impl LazySize {
         }
     }
 
+    /// Adds this size to another size.
     #[inline]
     pub fn combine(self, rhs: LazySize) -> LazySize {
         match (self, rhs) {
