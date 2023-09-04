6c7740b8ae2c52f0fe2c00fe7c0351f26f448784
Thalia Archibald <thalia@archibald.dev>
2023-09-03 19:08:10 -0700

    Parse burghard::Inst from lines

--- wspace-syntax/src/wsa/burghard/ast.rs
+++ wspace-syntax/src/wsa/burghard/ast.rs
@@ -1,222 +1,293 @@
-use std::borrow::Cow;
 use std::str::FromStr;
 
 use phf::{phf_map, Map};
 use rug::Integer;
+use strum::EnumDiscriminants;
 use uncased::UncasedStr;
 
-use crate::parse::{read_integer_haskell, ReadIntegerError};
+use crate::parse::read_integer_haskell;
+use crate::wsa::burghard::Word;
 
-macro_rules! map(
-    ( , $then:tt) => {};
-    ($optional:tt, $then:tt) => { $then };
-);
-
-macro_rules! insts {
-    (
-        $(#[$inst_attr:meta])* $inst_vis:vis enum $Inst:ident
-        $(#[$opcode_attr:meta])* $opcode_vis:vis enum $Opcode:ident
-        $([$mnemonic:literal $(, $mnemonic_alias:literal)*] =>
-            $Op:ident $(($($param:ty),+))?),* $(,)?
-    ) => {
-        $(#[$inst_attr])*
-        #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-        $inst_vis enum $Inst {
-            $($Op $(($($param),+))?),*
-        }
-
-        $(#[$opcode_attr])*
-        #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
-        $opcode_vis enum $Opcode {
-            $($Op),*
-        }
-
-        impl Inst {
-            pub fn opcode(&self) -> Opcode {
-                match self {
-                    $($Inst::$Op $(($(map!($param, _)),+))? => $Opcode::$Op,)*
-                }
-            }
-        }
-
-        impl FromStr for $Opcode {
-            type Err = ();
-
-            fn from_str(s: &str) -> Result<Self, Self::Err> {
-                static MNEMONICS: Map<&'static UncasedStr, $Opcode> = phf_map! {
-                    $(UncasedStr::new($mnemonic) => $Opcode::$Op,
-                        $(UncasedStr::new($mnemonic_alias) => $Opcode::$Op,)*)*
-                };
-
-                MNEMONICS.get(&UncasedStr::new(s)).copied().ok_or(())
-            }
-        }
-
-        impl AsRef<str> for $Opcode {
-            fn as_ref(&self) -> &str {
-                match *self {
-                    $($Opcode::$Op => $mnemonic,)*
-                }
-            }
-        }
-    };
-}
-
-insts! {
-    /// Instruction in the Burghard Whitespace assembly dialect.
-    pub enum Inst
-
-    /// Instruction opcode in the Burghard Whitespace assembly dialect.
-    ///
-    /// This is [`Inst`] without arguments.
-    pub enum Opcode
-
+/// Instruction in the Burghard Whitespace assembly dialect.
+#[derive(Clone, Debug, EnumDiscriminants, PartialEq, Eq, Hash)]
+#[strum_discriminants(name(Opcode), derive(Hash))]
+pub enum Inst<'a> {
     // Standard instructions
-    ["push"] => Push(IntegerValue),
-    ["doub"] => Doub,
-    ["swap"] => Swap,
-    ["pop"] => Pop,
-    ["add"] => Add(Option<IntegerValue>),
-    ["sub"] => Sub(Option<IntegerValue>),
-    ["mul"] => Mul(Option<IntegerValue>),
-    ["div"] => Div(Option<IntegerValue>),
-    ["mod"] => Mod(Option<IntegerValue>),
-    ["store"] => Store(Option<IntegerValue>),
-    ["retrive"] => Retrive(Option<IntegerValue>),
-    ["label"] => Label(String),
-    ["call"] => Call(String),
-    ["jump"] => Jump(String),
-    ["jumpz"] => JumpZ(String),
-    ["jumpn"] => JumpN(String),
-    ["ret"] => Ret,
-    ["exit"] => Exit,
-    ["outc"] => OutC,
-    ["outn"] => OutN,
-    ["inc"] => InC,
-    ["inn"] => InN,
+    Push(ParsedInteger<'a>),
+    Doub,
+    Swap,
+    Pop,
+    Add(Option<ParsedInteger<'a>>),
+    Sub(Option<ParsedInteger<'a>>),
+    Mul(Option<ParsedInteger<'a>>),
+    Div(Option<ParsedInteger<'a>>),
+    Mod(Option<ParsedInteger<'a>>),
+    Store(Option<ParsedInteger<'a>>),
+    Retrive(Option<ParsedInteger<'a>>),
+    Label(Word<'a>),
+    Call(Word<'a>),
+    Jump(Word<'a>),
+    JumpZ(Word<'a>),
+    JumpN(Word<'a>),
+    Ret,
+    Exit,
+    OutC,
+    OutN,
+    InC,
+    InN,
     // Extension instructions
-    ["debug_printstack"] => DebugPrintStack,
-    ["debug_printheap"] => DebugPrintHeap,
+    DebugPrintStack,
+    DebugPrintHeap,
     // Macro instructions
-    ["pushs"] => PushS(StringValue),
-    ["jumpp"] => JumpP(String),
-    ["jumpnz"] => JumpNZ(String),
-    ["jumppz"] => JumpPZ(String),
-    ["jumpnp", "jumppn"] => JumpNP(String),
-    ["include"] => Include(String),
-    ["test"] => Test(IntegerValue),
-    ["valueinteger"] => ValueI(Variable, IntegerValue),
-    ["valuestring"] => ValueS(Variable, StringValue),
+    PushS(ParsedString<'a>),
+    JumpP(Word<'a>),
+    JumpNZ(Word<'a>),
+    JumpPZ(Word<'a>),
+    JumpNP(Word<'a>),
+    Include(Word<'a>),
+    Test(ParsedInteger<'a>),
+    ValueI(ParsedVariable<'a>, ParsedInteger<'a>),
+    ValueS(ParsedVariable<'a>, ParsedString<'a>),
     // Preprocessor instructions
-    ["option"] => Option(String),
-    ["ifoption"] => IfOption(String),
-    ["elseifoption"] => ElseIfOption(String),
-    ["elseoption"] => ElseOption,
-    ["endoption"] => EndOption,
+    Option(Word<'a>),
+    IfOption(Word<'a>),
+    ElseIfOption(Word<'a>),
+    ElseOption,
+    EndOption,
+    /// A malformed instruction
+    Error(InstError<'a>),
 }
 
-type Word<'a> = Cow<'a, str>;
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+
+pub enum InstError<'a> {
+    Arity(Vec<Word<'a>>),
+}
+
+impl Inst<'_> {
+    #[inline]
+    pub fn opcode(&self) -> Opcode {
+        Opcode::from(self)
+    }
+}
+
+impl FromStr for Opcode {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        static MNEMONICS: Map<&'static UncasedStr, Opcode> = phf_map! {
+            UncasedStr::new("push") => Opcode::Push,
+            UncasedStr::new("doub") => Opcode::Doub,
+            UncasedStr::new("swap") => Opcode::Swap,
+            UncasedStr::new("pop") => Opcode::Pop,
+            UncasedStr::new("add") => Opcode::Add,
+            UncasedStr::new("sub") => Opcode::Sub,
+            UncasedStr::new("mul") => Opcode::Mul,
+            UncasedStr::new("div") => Opcode::Div,
+            UncasedStr::new("mod") => Opcode::Mod,
+            UncasedStr::new("store") => Opcode::Store,
+            UncasedStr::new("retrive") => Opcode::Retrive,
+            UncasedStr::new("label") => Opcode::Label,
+            UncasedStr::new("call") => Opcode::Call,
+            UncasedStr::new("jump") => Opcode::Jump,
+            UncasedStr::new("jumpz") => Opcode::JumpZ,
+            UncasedStr::new("jumpn") => Opcode::JumpN,
+            UncasedStr::new("ret") => Opcode::Ret,
+            UncasedStr::new("exit") => Opcode::Exit,
+            UncasedStr::new("outc") => Opcode::OutC,
+            UncasedStr::new("outn") => Opcode::OutN,
+            UncasedStr::new("inc") => Opcode::InC,
+            UncasedStr::new("inn") => Opcode::InN,
+            UncasedStr::new("debug_printstack") => Opcode::DebugPrintStack,
+            UncasedStr::new("debug_printheap") => Opcode::DebugPrintHeap,
+            UncasedStr::new("pushs") => Opcode::PushS,
+            UncasedStr::new("jumpp") => Opcode::JumpP,
+            UncasedStr::new("jumpnz") => Opcode::JumpNZ,
+            UncasedStr::new("jumppz") => Opcode::JumpPZ,
+            // JumpNP has two mnemonics:
+            UncasedStr::new("jumpnp") => Opcode::JumpNP,
+            UncasedStr::new("jumppn") => Opcode::JumpNP,
+            UncasedStr::new("include") => Opcode::Include,
+            UncasedStr::new("test") => Opcode::Test,
+            UncasedStr::new("valueinteger") => Opcode::ValueI,
+            UncasedStr::new("valuestring") => Opcode::ValueS,
+            UncasedStr::new("option") => Opcode::Option,
+            UncasedStr::new("ifoption") => Opcode::IfOption,
+            UncasedStr::new("elseifoption") => Opcode::ElseIfOption,
+            UncasedStr::new("elseoption") => Opcode::ElseOption,
+            UncasedStr::new("endoption") => Opcode::EndOption,
+        };
+
+        MNEMONICS.get(&UncasedStr::new(s)).copied().ok_or(())
+    }
+}
+
+impl Opcode {
+    pub fn mnemonic(&self) -> Option<&'static str> {
+        match *self {
+            Opcode::Push => Some("push"),
+            Opcode::Doub => Some("doub"),
+            Opcode::Swap => Some("swap"),
+            Opcode::Pop => Some("pop"),
+            Opcode::Add => Some("add"),
+            Opcode::Sub => Some("sub"),
+            Opcode::Mul => Some("mul"),
+            Opcode::Div => Some("div"),
+            Opcode::Mod => Some("mod"),
+            Opcode::Store => Some("store"),
+            Opcode::Retrive => Some("retrive"),
+            Opcode::Label => Some("label"),
+            Opcode::Call => Some("call"),
+            Opcode::Jump => Some("jump"),
+            Opcode::JumpZ => Some("jumpz"),
+            Opcode::JumpN => Some("jumpn"),
+            Opcode::Ret => Some("ret"),
+            Opcode::Exit => Some("exit"),
+            Opcode::OutC => Some("outc"),
+            Opcode::OutN => Some("outn"),
+            Opcode::InC => Some("inc"),
+            Opcode::InN => Some("inn"),
+            Opcode::DebugPrintStack => Some("debug_printstack"),
+            Opcode::DebugPrintHeap => Some("debug_printheap"),
+            Opcode::PushS => Some("pushs"),
+            Opcode::JumpP => Some("jumpp"),
+            Opcode::JumpNZ => Some("jumpnz"),
+            Opcode::JumpPZ => Some("jumppz"),
+            Opcode::JumpNP => Some("jumpnp"),
+            Opcode::Include => Some("include"),
+            Opcode::Test => Some("test"),
+            Opcode::ValueI => Some("valueinteger"),
+            Opcode::ValueS => Some("valuestring"),
+            Opcode::Option => Some("option"),
+            Opcode::IfOption => Some("ifoption"),
+            Opcode::ElseIfOption => Some("elseifoption"),
+            Opcode::ElseOption => Some("elseoption"),
+            Opcode::EndOption => Some("endoption"),
+            Opcode::Error => None,
+        }
+    }
+
+    pub fn with_args<'a>(self, words: &mut Vec<Word<'a>>) -> Inst<'a> {
+        let mut words = words.drain(..);
+        macro_rules! next(() => { words.next().unwrap().into() });
+        match (self, words.len()) {
+            (Opcode::Push, 1) => Inst::Push(next!()),
+            (Opcode::PushS, 1) => Inst::PushS(next!()),
+            (Opcode::Doub, 0) => Inst::Doub,
+            (Opcode::Swap, 0) => Inst::Swap,
+            (Opcode::Pop, 0) => Inst::Pop,
+            (Opcode::Add, 0) => Inst::Add(None),
+            (Opcode::Sub, 0) => Inst::Sub(None),
+            (Opcode::Mul, 0) => Inst::Mul(None),
+            (Opcode::Div, 0) => Inst::Div(None),
+            (Opcode::Mod, 0) => Inst::Mod(None),
+            (Opcode::Add, 1) => Inst::Add(Some(next!())),
+            (Opcode::Sub, 1) => Inst::Sub(Some(next!())),
+            (Opcode::Mul, 1) => Inst::Mul(Some(next!())),
+            (Opcode::Div, 1) => Inst::Div(Some(next!())),
+            (Opcode::Mod, 1) => Inst::Mod(Some(next!())),
+            (Opcode::Store, 0) => Inst::Store(None),
+            (Opcode::Retrive, 0) => Inst::Retrive(None),
+            (Opcode::Store, 1) => Inst::Store(Some(next!())),
+            (Opcode::Retrive, 1) => Inst::Retrive(Some(next!())),
+            (Opcode::Label, 1) => Inst::Label(next!()),
+            (Opcode::Call, 1) => Inst::Call(next!()),
+            (Opcode::Jump, 1) => Inst::Jump(next!()),
+            (Opcode::JumpZ, 1) => Inst::JumpZ(next!()),
+            (Opcode::JumpN, 1) => Inst::JumpN(next!()),
+            (Opcode::JumpP, 1) => Inst::JumpP(next!()),
+            (Opcode::JumpNZ, 1) => Inst::JumpNZ(next!()),
+            (Opcode::JumpPZ, 1) => Inst::JumpPZ(next!()),
+            (Opcode::JumpNP, 1) => Inst::JumpNP(next!()),
+            (Opcode::Ret, 0) => Inst::Ret,
+            (Opcode::Exit, 0) => Inst::Exit,
+            (Opcode::OutC, 0) => Inst::OutC,
+            (Opcode::OutN, 0) => Inst::OutN,
+            (Opcode::InC, 0) => Inst::InC,
+            (Opcode::InN, 0) => Inst::InN,
+            (Opcode::DebugPrintStack, 0) => Inst::DebugPrintStack,
+            (Opcode::DebugPrintHeap, 0) => Inst::DebugPrintHeap,
+            (Opcode::Include, 1) => Inst::Include(next!()),
+            (Opcode::Test, 1) => Inst::Test(next!()),
+            (Opcode::ValueI, 2) => Inst::ValueI(next!(), next!()),
+            (Opcode::ValueS, 2) => Inst::ValueS(next!(), next!()),
+            (Opcode::Option, 1) => Inst::Option(next!()),
+            (Opcode::IfOption, 1) => Inst::IfOption(next!()),
+            (Opcode::ElseIfOption, 1) => Inst::ElseIfOption(next!()),
+            (Opcode::ElseOption, 0) => Inst::ElseOption,
+            (Opcode::EndOption, 0) => Inst::EndOption,
+            _ => Inst::Error(InstError::Arity(words.collect())),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct ParsedWord<'a, T> {
+    pub word: Word<'a>,
+    pub value: T,
+}
+
+type ParsedInteger<'a> = ParsedWord<'a, IntegerValue>;
+type ParsedString<'a> = ParsedWord<'a, StringValue>;
+type ParsedVariable<'a> = ParsedWord<'a, VariableValue>;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum IntegerValue {
     Literal(Integer),
-    Variable(Variable),
+    LiteralError,
+    Variable,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum StringValue {
-    Literal(String),
-    Variable(Variable),
+    Literal,
+    Variable,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct Variable(String);
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum ArgError {
-    Arity,
-    ParseInteger(ReadIntegerError),
-    IdentMissingUnderscore,
+pub enum VariableValue {
+    Variable,
+    Error,
 }
 
-impl Opcode {
-    pub fn with_args(self, lhs: Option<Word<'_>>, rhs: Option<Word<'_>>) -> Result<Inst, ArgError> {
-        match (self, lhs, rhs) {
-            (Opcode::Push, Some(n), None) => Ok(Inst::Push(n.try_into()?)),
-            (Opcode::PushS, Some(s), None) => Ok(Inst::PushS(s.into())),
-            (Opcode::Doub, None, None) => Ok(Inst::Doub),
-            (Opcode::Swap, None, None) => Ok(Inst::Swap),
-            (Opcode::Pop, None, None) => Ok(Inst::Pop),
-            (Opcode::Add, n, None) => Ok(Inst::Add(n.map(Word::try_into).transpose()?)),
-            (Opcode::Sub, n, None) => Ok(Inst::Sub(n.map(Word::try_into).transpose()?)),
-            (Opcode::Mul, n, None) => Ok(Inst::Mul(n.map(Word::try_into).transpose()?)),
-            (Opcode::Div, n, None) => Ok(Inst::Div(n.map(Word::try_into).transpose()?)),
-            (Opcode::Mod, n, None) => Ok(Inst::Mod(n.map(Word::try_into).transpose()?)),
-            (Opcode::Store, n, None) => Ok(Inst::Store(n.map(Word::try_into).transpose()?)),
-            (Opcode::Retrive, n, None) => Ok(Inst::Retrive(n.map(Word::try_into).transpose()?)),
-            (Opcode::Label, Some(l), None) => Ok(Inst::Label(l.into())),
-            (Opcode::Call, Some(l), None) => Ok(Inst::Call(l.into())),
-            (Opcode::Jump, Some(l), None) => Ok(Inst::Jump(l.into())),
-            (Opcode::JumpZ, Some(l), None) => Ok(Inst::JumpZ(l.into())),
-            (Opcode::JumpN, Some(l), None) => Ok(Inst::JumpN(l.into())),
-            (Opcode::JumpP, Some(l), None) => Ok(Inst::JumpP(l.into())),
-            (Opcode::JumpNZ, Some(l), None) => Ok(Inst::JumpNZ(l.into())),
-            (Opcode::JumpPZ, Some(l), None) => Ok(Inst::JumpPZ(l.into())),
-            (Opcode::JumpNP, Some(l), None) => Ok(Inst::JumpNP(l.into())),
-            (Opcode::Ret, None, None) => Ok(Inst::Ret),
-            (Opcode::Exit, None, None) => Ok(Inst::Exit),
-            (Opcode::OutC, None, None) => Ok(Inst::OutC),
-            (Opcode::OutN, None, None) => Ok(Inst::OutN),
-            (Opcode::InC, None, None) => Ok(Inst::InC),
-            (Opcode::InN, None, None) => Ok(Inst::InN),
-            (Opcode::DebugPrintStack, None, None) => Ok(Inst::DebugPrintStack),
-            (Opcode::DebugPrintHeap, None, None) => Ok(Inst::DebugPrintHeap),
-            (Opcode::Include, Some(f), None) => Ok(Inst::Include(f.into())),
-            (Opcode::Test, Some(n), None) => Ok(Inst::Test(n.try_into()?)),
-            (Opcode::ValueI, Some(v), Some(n)) => Ok(Inst::ValueI(v.try_into()?, n.try_into()?)),
-            (Opcode::ValueS, Some(v), Some(s)) => Ok(Inst::ValueS(v.try_into()?, s.into())),
-            (Opcode::Option, Some(opt), None) => Ok(Inst::Option(opt.into())),
-            (Opcode::IfOption, Some(opt), None) => Ok(Inst::IfOption(opt.into())),
-            (Opcode::ElseIfOption, Some(opt), None) => Ok(Inst::ElseIfOption(opt.into())),
-            (Opcode::ElseOption, None, None) => Ok(Inst::ElseOption),
-            (Opcode::EndOption, None, None) => Ok(Inst::EndOption),
-            _ => Err(ArgError::Arity),
-        }
+impl Word<'_> {
+    pub fn is_variable(&self) -> bool {
+        matches!(self.text.as_bytes(), [b'_', ..])
     }
 }
 
-impl TryFrom<Word<'_>> for Variable {
-    type Error = ArgError;
-
-    fn try_from(word: Word<'_>) -> Result<Self, Self::Error> {
-        if let [b'_', ..] = word.as_bytes() {
-            Ok(Variable(word[1..].into()))
+impl<'a> From<Word<'a>> for ParsedInteger<'a> {
+    fn from(word: Word<'a>) -> Self {
+        let value = if word.is_variable() {
+            IntegerValue::Variable
         } else {
-            Err(ArgError::IdentMissingUnderscore)
-        }
+            match read_integer_haskell(&word.text) {
+                Ok(n) => IntegerValue::Literal(n),
+                Err(_) => IntegerValue::LiteralError,
+            }
+        };
+        ParsedInteger { word, value }
     }
 }
 
-impl TryFrom<Word<'_>> for IntegerValue {
-    type Error = ArgError;
-
-    fn try_from(word: Word<'_>) -> Result<Self, Self::Error> {
-        Variable::try_from(word.clone())
-            .map(IntegerValue::Variable)
-            .or_else(|_| Ok(IntegerValue::Literal(read_integer_haskell(&word)?)))
+impl<'a> From<Word<'a>> for ParsedString<'a> {
+    fn from(word: Word<'a>) -> Self {
+        let value = if word.is_variable() {
+            StringValue::Variable
+        } else {
+            StringValue::Literal
+        };
+        ParsedString { word, value }
     }
 }
 
-impl From<Word<'_>> for StringValue {
-    fn from(word: Word<'_>) -> Self {
-        Variable::try_from(word.clone())
-            .map(StringValue::Variable)
-            .unwrap_or_else(|_| StringValue::Literal(word.into()))
-    }
-}
-
-impl From<ReadIntegerError> for ArgError {
-    fn from(err: ReadIntegerError) -> Self {
-        ArgError::ParseInteger(err)
+impl<'a> From<Word<'a>> for ParsedVariable<'a> {
+    fn from(word: Word<'a>) -> Self {
+        let value = if word.is_variable() {
+            VariableValue::Variable
+        } else {
+            VariableValue::Error
+        };
+        ParsedVariable { word, value }
     }
 }
