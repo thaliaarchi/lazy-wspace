120f31960172b98ad8f7d4e833b55fbbdf574cc1
Thalia Archibald <thalia@archibald.dev>
2023-10-03 04:31:29 -0700

    Use enum dispatch instead of dyn dispatch for bit lexing

--- wspace-syntax/src/ws/lex/bit.rs
+++ wspace-syntax/src/ws/lex/bit.rs
@@ -1,8 +1,9 @@
+use std::iter::FusedIterator;
 use std::mem;
 
 use bitvec::{
     order::{BitOrder, LocalBits, Lsb0, Msb0},
-    slice::{BitSlice, Iter as BitSliceIter},
+    slice::{BitSlice, Iter as BitIter},
     store::BitStore,
 };
 
@@ -10,18 +11,24 @@ use crate::ws::lex::{Lexer, Span};
 use crate::ws::Token;
 
 #[derive(Clone, Debug)]
-pub struct BitLexer {
+pub struct BitSyntax {
     order: DynBitOrder,
 }
 
 #[derive(Clone, Debug)]
-pub struct BitIter<'s, T = usize, O = Lsb0>
+pub struct BitLexer<'a, T = u8, O = Lsb0>
 where
     T: BitStore,
     O: BitOrder,
 {
-    iter: BitSliceIter<'s, T, O>,
+    iter: BitIter<'a, T, O>,
     len: usize,
+}
+
+#[derive(Clone, Debug)]
+pub enum DynBitLexer<'a, T: BitStore = u8> {
+    Lsb0(BitLexer<'a, T, Lsb0>),
+    Msb0(BitLexer<'a, T, Msb0>),
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -34,46 +41,53 @@ pub enum DynBitOrder {
     LocalBits,
 }
 
-impl BitLexer {
+impl BitSyntax {
     #[inline]
     pub fn new(order: DynBitOrder) -> Self {
-        BitLexer { order }
+        BitSyntax { order }
     }
 
     #[inline]
-    pub fn lex<'s>(&self, src: &'s [u8]) -> Box<dyn Lexer + 's> {
+    pub fn lex<'a, T: BitStore>(&self, src: &'a [T]) -> DynBitLexer<'a, T> {
         match self.order {
-            DynBitOrder::Lsb0 => Box::new(BitIter::<u8, Lsb0>::new(src)),
-            DynBitOrder::Msb0 => Box::new(BitIter::<u8, Msb0>::new(src)),
-            DynBitOrder::LocalBits => Box::new(BitIter::<u8, LocalBits>::new(src)),
+            DynBitOrder::Lsb0 => DynBitLexer::from(BitLexer::<T, Lsb0>::new(src)),
+            DynBitOrder::Msb0 => DynBitLexer::from(BitLexer::<T, Msb0>::new(src)),
+            DynBitOrder::LocalBits => DynBitLexer::from(BitLexer::<T, LocalBits>::new(src)),
         }
+    }
+
+    #[inline]
+    pub fn order(&self) -> DynBitOrder {
+        self.order
     }
 }
 
-impl<'s, T: BitStore, O: BitOrder> BitIter<'s, T, O> {
+impl<'a, T: BitStore, O: BitOrder> BitLexer<'a, T, O> {
     #[inline]
-    pub fn new(src: &'s [T]) -> Self {
+    pub fn new(src: &'a [T]) -> Self {
         let mut bits = BitSlice::from_slice(src);
         // Trim trailing zeros in the last element.
         let tz = bits.trailing_zeros();
         if 0 < tz && tz < mem::size_of::<T>() * 8 {
             bits = &bits[..bits.len() - tz];
         }
-        Self::from_bitslice(bits)
+        BitLexer::from(bits)
     }
+}
 
+impl<'a, T: BitStore, O: BitOrder> From<&'a BitSlice<T, O>> for BitLexer<'a, T, O> {
     #[inline]
-    pub fn from_bitslice(bits: &'s BitSlice<T, O>) -> Self {
-        BitIter {
-            iter: bits.into_iter(),
+    fn from(bits: &'a BitSlice<T, O>) -> Self {
+        BitLexer {
+            iter: bits.iter(),
             len: bits.len(),
         }
     }
 }
 
-impl<'s, T: BitStore, O: BitOrder> Lexer for BitIter<'s, T, O> {}
+impl<T: BitStore, O: BitOrder> Lexer for BitLexer<'_, T, O> {}
 
-impl<'s, T: BitStore, O: BitOrder> Iterator for BitIter<'s, T, O> {
+impl<T: BitStore, O: BitOrder> Iterator for BitLexer<'_, T, O> {
     type Item = (Token, Span);
 
     #[inline]
@@ -97,5 +111,45 @@ impl<'s, T: BitStore, O: BitOrder> Iterator for BitIter<'s, T, O> {
         // Lower bound is the case of all two-bit tokens, rounded up, and upper
         // bound is all one-bit tokens.
         ((self.iter.len() + 1) / 2, Some(self.iter.len()))
+    }
+}
+
+impl<T: BitStore, O: BitOrder> FusedIterator for BitLexer<'_, T, O> {}
+
+impl<T: BitStore> Lexer for DynBitLexer<'_, T> {}
+
+impl<T: BitStore> Iterator for DynBitLexer<'_, T> {
+    type Item = (Token, Span);
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            DynBitLexer::Lsb0(inner) => inner.next(),
+            DynBitLexer::Msb0(inner) => inner.next(),
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        match self {
+            DynBitLexer::Lsb0(inner) => inner.size_hint(),
+            DynBitLexer::Msb0(inner) => inner.size_hint(),
+        }
+    }
+}
+
+impl<T: BitStore> FusedIterator for DynBitLexer<'_, T> {}
+
+impl<'a, T: BitStore> From<BitLexer<'a, T, Lsb0>> for DynBitLexer<'a, T> {
+    #[inline]
+    fn from(lex: BitLexer<'a, T, Lsb0>) -> Self {
+        DynBitLexer::Lsb0(lex)
+    }
+}
+
+impl<'a, T: BitStore> From<BitLexer<'a, T, Msb0>> for DynBitLexer<'a, T> {
+    #[inline]
+    fn from(lex: BitLexer<'a, T, Msb0>) -> Self {
+        DynBitLexer::Msb0(lex)
     }
 }
