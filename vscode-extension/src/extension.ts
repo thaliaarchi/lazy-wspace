274c15ae3a2c83e928758db6765d3e6a51755b8b
Thalia Archibald <thalia@archibald.dev>
2023-11-04 12:34:14 -0700

    Start VS Code extension

---
+++ vscode-extension/src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+// Called when the extension is activated.
+export function activate(_context: vscode.ExtensionContext) {
+    console.log('Whitespace extension has been activated');
+}
+
+// Called when the extension is deactivated.
+export function deactivate() {}
